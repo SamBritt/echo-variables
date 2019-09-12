0ff2209a55b97d8a2852fe43a6d784dc51eb2036
SamBritt <sam.britt2@yahoo.com>
2019-09-12 09:58:54 -0500

    added php inline with html. Taking values from query parameters

--- index.php
+++ index.php
@@ -1,12 +1,45 @@
-<?php
+<html>
 
-$greeting = "What's up";
-$name = "Sam";
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
 
-echo 'my first line of PHP';
-//php -S localhost: whatever port
-//serves php to browser
+<body>
 
-//use nl2br with \n sequence to break lines
-echo nl2br("\n $greeting");
-echo nl2br("\n $greeting"." $name?");
+    <header>
+        <h1>
+            <?php
+            
+
+            $greeting = "What's up";
+            //super global
+            //Gets value associated with url parameter: e.g. name=Sam
+            //NOT SECURE. Allows people to inject code >:O
+            //Wrap w/htmlspecialchars function to 'sanitize input'. Guilt until proven innocent!
+            $name = htmlspecialchars($_GET['name']);
+
+            //php -S localhost: whatever port
+            //serves php to browser
+
+            echo "Goodmorning, {$name}";
+
+            ?>
+        </h1>
+    </header>
+    <section>
+        <div>
+            <p>
+                <!-- shorthand for a php tag echoing something upon execution -->
+                <?=
+                //use nl2br with \n sequence to break lines
+                
+                nl2br("\n $greeting" . " $name?");
+                ?>
+            </p>
+        </div>
+    </section>
+
+</body>
+
+</html>
